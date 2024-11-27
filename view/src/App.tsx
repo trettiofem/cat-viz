455fbb2d8b2561b2a1b7eb1357c095c8602cf451
Jacob Johansson <jacob@trettiofem.com>
2024-11-27 15:42:54 +0100

    Started work on search function

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -2,6 +2,9 @@ import { CallGraphContainer } from "@/components/call-graph-container";
 import { SettingsMenu } from "./components/settings-menu";
 import { fetchCallGraph } from "./lib/call-graph";
 import { RootStateProvider } from "./lib/state/provider";
+import { Search } from "./components/search";
+import { DepthSelector } from "./components/depth-selector";
+
 
 function App() {
     return (
@@ -11,8 +14,10 @@ function App() {
                     graph={fetchCallGraph()}
                     className="w-dvw h-dvh"
                 />
-                <div className="absolute top-0 left-0 m-4">
+                <div className="absolute top-0 left-0 m-4 flex flex-row gap-2">
+                    <Search />
                     <SettingsMenu />
+                    <DepthSelector />
                 </div>
             </div>
         </RootStateProvider>
